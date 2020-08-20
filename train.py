6173a2c47508717d9d4067d2f64cbc0fa46f859d
Steven Song <steve@Steve-Macbook.local>
2020-08-20 12:36:49 +0900

    add custom training

--- train.py
+++ train.py
@@ -11,62 +11,64 @@ from create_data import download_data
 ds_train, ds_test = download_data(dataset_name='cifar100', batch_size=128)
 
 
-model = ReXNet(classes=100)
+model = MobileNetV2(classes=100)
 model.compile(
     optimizer=keras.optimizers.RMSprop(),  # Optimizer
-    loss=keras.losses.SparseCategoricalCrossentropy(),
+    loss=keras.losses.CategoricalCrossentropy(),
     metrics=[keras.metrics.SparseCategoricalAccuracy()],
 )
 
 #keras.utils.plot_model(model, "rexnet.png")
 
-checkpoint_path = "./checkpoints/rexnet.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
 
-# Create a callback that saves the model's weights
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+import pickle
 
 train_loss_results = []
 train_accuracy_results = []
 
-num_epochs = 201
+num_epochs = 15
+
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets, training=True)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+def loss(model, x, y, training):
+  # training=training is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+    y_ = model(x, training=training)
+    loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    return loss_object(y_true=y, y_pred=y_)
 
 for epoch in range(num_epochs):
-  epoch_loss_avg = tf.keras.metrics.Mean()
-  epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    print("Starting Epoch {:03d}".format(epoch))
+    epoch_loss_avg = tf.keras.metrics.Mean()
+    epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
 
-  # Training loop - using batches of 32
-  for x, y in train_dataset:
-    # Optimize the model
-    loss_value, grads = grad(model, x, y)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    # Training loop - using batches of 32
+    for x, y in ds_train:
+        # Optimize the model
+        loss_value, grads = grad(model, x, y)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-    # Track progress
-    epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-    # Compare predicted label to actual label
-    # training=True is needed only if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
-    epoch_accuracy.update_state(y, model(x, training=True))
+        # Track progress
+        epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+        # Compare predicted label to actual label
+        # training=True is needed only if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        epoch_accuracy.update_state(y, model(x, training=True))
 
-  # End epoch
-  train_loss_results.append(epoch_loss_avg.result())
-  train_accuracy_results.append(epoch_accuracy.result())
-
-  if epoch % 50 == 0:
+    # End epoch
+    train_loss_results.append(epoch_loss_avg.result())
+    train_accuracy_results.append(epoch_accuracy.result())
     print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
-hist = model.fit(ds_train, epochs=10, validation_data=ds_test,
-                    callbacks=[cp_callback])
+                                                            epoch_loss_avg.result(),
+                                                            epoch_accuracy.result()))
+        
+#hist = model.fit(ds_train, epochs=20, validation_data=ds_test, verbose=1)
 
 
-loss, acc = model.evaluate(ds_test, verbose=2)
-print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
-
-# Save history
-import pickle
-
-with open('rexnet_hist.pkl', 'wb') as f:
-    pickle.dump(hist)
+with open('train_loss_rexnet.pkl') as f:
+    pickle.dump(train_loss_results)
+with open('test_loss_rexnet.pkl') as f:
+    pickle.dump(test_loss_results)
